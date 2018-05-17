5f7c24fcecbfd03bf2856b1307800342dedc9c4a
Paweł Powroźnik <pawel.powroznik21@gmail.com>
2018-05-17 19:07:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var height = "74"
 if (name === "Susan") {
   console.log("The name is correct")
 } else {
-  console.log("Expect to ")
+  console.log("Expect the name to be ")
 }
 
 // Don't worry about this
